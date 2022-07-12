283df88b2a4608c0faa98419ab980a457fd87430
satrams <mememaster420noscope360@gmail.com>
2022-07-12 16:23:10 -0400

    Template Upsert + Display Refactor + Error Handler
    Implemented Template Upsert
    Implemented method for displaying multiple application components using a switch statement (will become an enum later)
    implemented a better error handling system to not crash the system upon errors by pushing them to the frontend so they can be logged

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { invoke } from '@tauri-apps/api'
 
-function App() {
+interface BaseParams {
+  setState: (state: number) => void,
+}
+const AuthPage = (props: BaseParams) => {
 
-  invoke('get_auth_url').then((url) => {
-    document.getElementById("auth_url")?.setAttribute("href", String(url));
-  })
+  const [authUrl, setAuthUrl] = useState("");
+
+  //We need to wrap this in useEffect so it only happens on first render, otherwise, on state change, react would rerender again and loop
+  useEffect(() => {
+    invoke('get_auth_url').then((url) => {
+      setAuthUrl(String(url));
+    })
+  }, []);
+
+
   return (
-    <div className="App">
+    < div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <a
             id="auth_url"
             className='auth_url'
-            href=""
+            href={authUrl}
             target="_blank"
           >
             Obtain Auth Key
@@ -25,16 +35,54 @@ function App() {
         <p>
           <input type='text' id="auth-code" placeholder='Paste authentication code here' />
           <button onClick={() => {
-            let code = (document.getElementById("auth-code") as HTMLInputElement).value;
-            invoke('finalize_auth', { code });
+            let code: string = (document.getElementById("auth-code") as HTMLInputElement).value;
+            invoke('finalize_auth', { code })
+              .catch((err) => console.error(err))
+              .then(() => props.setState(1));
           }}>Submit</button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
       </header>
+    </div >
+  )
+};
+
+const MainPage = (props: BaseParams) => {
+
+  useEffect(() => {
+    invoke('upsert_template').catch((err) => { console.error(err) });
+  }, []);
+
+  return (
+    < div className="App" >
+      <div className='nav'>
+      </div>
     </div>
   )
+};
+
+const ErrorPage = () => {
+  return (
+    <div>Error, this page occurs when the app attemps to swap to a page that doesn't exist. Please open an issue if you ever see this page</div>
+  )
+}
+
+function App() {
+
+  const [state, setState] = useState(0);
+
+  //depending on the state of the app (set through an enum, the app will display a specific page)
+  switch (state) {
+    case 0:
+      return <AuthPage setState={setState} />;
+    case 1:
+      return <MainPage setState={setState} />;
+    default:
+      return <ErrorPage />
+  }
+
 }
 
 export default App
