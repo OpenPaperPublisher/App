72c4f28e884d8de4d7fe80764fa55359f3891098
satrams <mememaster420noscope360@gmail.com>
2022-07-29 17:16:40 -0400

    Added feature to prevent button from restarting program

--- src/pages/components/PublishDrawer.tsx
+++ src/pages/components/PublishDrawer.tsx
@@ -25,7 +25,7 @@ const PublishDrawer = ({ show, folderPath, togglePublishDrawer }: any) => {
             >
                 Close
             </div>
-            <form className="mt-4">
+            <form className="mt-4" onSubmit={(e) => { e.preventDefault() }}>
                 <div>
                     <input type="button" className="hover:cursor-pointer" value="Select Local Folder" onClick={() => {
                         open({ directory: true }).then((result) => {
@@ -42,7 +42,7 @@ const PublishDrawer = ({ show, folderPath, togglePublishDrawer }: any) => {
                         className="mt-4 text-sm px-2 py-1 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
                         onClick={() => {
                             if (!path) return;
-                            invoke("export_folder", { folderPath, exportPath: path }).then((status) => {
+                            invoke("export_folder", { folderPath: (folderPath as String), exportPath: path }).then((status) => {
                                 console.log(status);
                             });
                         }}
