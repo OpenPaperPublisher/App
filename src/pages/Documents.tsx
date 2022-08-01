2f14c778db67c98503d3271a82370871752218be
Ryan Mahoney <ryan@virtuecenter.com>
2022-07-31 22:10:19 -0400

    WIP on changes to drawer

--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,77 +1,80 @@
-import React, { useEffect, useState } from "react";
-import { invoke } from '@tauri-apps/api'
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api";
 import Breadcrumbs from "./components/Breadcrumbs";
 import Table from "./components/Table";
 import Drawer from "./components/Drawer";
 import PublishDrawer from "./components/PublishDrawer";
-import { File, Folder } from "../dropbox_types";
+import { File } from "../dropbox_types";
 
 const Documents = ({ breadcrumbs, dispatch, folderPath }: any) => {
-    const [documents, setDocuments] = useState<File[]>([]);
-    const [activeDocument, setActiveDocument] = useState<File | null>(null);
-    const [publishDrawOpen, togglePublishDrawer] = useState(false);
+  const [documents, setDocuments] = useState<File[]>([]);
+  const [activeDocument, setActiveDocument] = useState<File | null>(null);
+  const [publishDrawOpen, togglePublishDrawer] = useState(false);
 
-    useEffect(() => {
-        invoke('list_files_in_dir', { target: folderPath as string }).then((documents) => {
-            console.log(documents as File[]);
-            setDocuments(documents as File[]);
-        }
-        );
-    }, [folderPath]);
+  console.log({ activeDocument });
 
-    return (
-        <div>
-            <Breadcrumbs paths={breadcrumbs} dispatch={dispatch} />
-            <div className="flex mb-4">
-                <h2 className="shrink-0 text-3xl font-bold text-gray-800 md:text-2xl self-center">
-                    Documents
-                </h2>
-                <button
-                    className="ml-4 text-sm px-2 py-1 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-                    onClick={() => togglePublishDrawer(!publishDrawOpen)}
-                >
-                    Publish Documents to Local
-                </button>
-            </div>
-            <Table headers={["Document Title"]}>
-                {documents.map((document) => (
-                    <tr key={document.path_lower} className="text-gray-700">
-                        <td className="px-4 py-3">
-                            <div className="flex items-center text-sm">
-                                <div
-                                    className="cursor-pointer"
-                                    onClick={() => {
-                                        if (activeDocument === null) {
-                                            setActiveDocument(document);
-                                        } else if (activeDocument === document) {
-                                            setActiveDocument(null);
-                                        } else {
-                                            setActiveDocument(null);
-                                            setActiveDocument(document);
-                                        }
-                                    }}
-                                >
-                                    {document.name}
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                ))}
-            </Table>
-
-            <Drawer
-                show={activeDocument !== null}
-                document={activeDocument}
-                setActiveDocument={setActiveDocument}
-            />
-
-            <PublishDrawer
-                show={publishDrawOpen === true}
-                folderPath={folderPath}
-                togglePublishDrawer={togglePublishDrawer}
-            />
-        </div>
+  useEffect(() => {
+    invoke("list_files_in_dir", { target: folderPath as string }).then(
+      (documents) => {
+        console.log(documents as File[]);
+        setDocuments(documents as File[]);
+      }
     );
+  }, [folderPath]);
+
+  return (
+    <div>
+      <Breadcrumbs paths={breadcrumbs} dispatch={dispatch} />
+      <div className="flex mb-4">
+        <h2 className="shrink-0 text-3xl font-bold text-gray-800 md:text-2xl self-center">
+          Documents
+        </h2>
+        <button
+          className="ml-4 text-sm px-2 py-1 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          onClick={() => togglePublishDrawer(!publishDrawOpen)}
+        >
+          Publish Documents to Local
+        </button>
+      </div>
+      <Table headers={["Document Title"]}>
+        {documents.map((document) => (
+          <tr key={document.path_lower} className="text-gray-700">
+            <td className="px-4 py-3">
+              <div className="flex items-center text-sm">
+                <div
+                  className="cursor-pointer"
+                  onClick={() => {
+                    if (activeDocument === null) {
+                      setActiveDocument(document);
+                    } else if (activeDocument === document) {
+                      setActiveDocument(null);
+                    } else {
+                      setActiveDocument(null);
+                      setActiveDocument(document);
+                    }
+                  }}
+                >
+                  {document.name}
+                </div>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </Table>
+
+      <Drawer
+        show={activeDocument !== null}
+        documentId={activeDocument !== null ? activeDocument["id"] : null}
+        setActiveDocument={setActiveDocument}
+      />
+
+      <PublishDrawer
+        show={publishDrawOpen === true}
+        folderPath={folderPath}
+        togglePublishDrawer={togglePublishDrawer}
+      />
+    </div>
+  );
 };
 
 export default Documents;
--- src/pages/components/Drawer.tsx
+++ src/pages/components/Drawer.tsx
@@ -1,196 +1,195 @@
+import { useEffect, useState } from "react";
 import { useSpring, animated } from "react-spring";
-import { invoke } from '@tauri-apps/api'
-import { useForm, Controller } from "react-hook-form";
-import { OPMproperties, File, Property } from '../../dropbox_types';
-import DatePicker from "react-datepicker";
+import { invoke } from "@tauri-apps/api";
+import { useForm } from "react-hook-form";
+import { OPMproperties, Property } from "../../dropbox_types";
 
 const inputClasses =
-    "block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring";
+  "block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring";
 
-// const fakeSingleMetadataApiResponse = async (documentPath: any) => {
-//     const [document] = fakeDocuments.filter(({ id }) => documentPath === id);
-//     return { document };
-// };
 
-const Drawer = ({ show, document, setActiveDocument }: { show: boolean, document: File | null, setActiveDocument: any }) => {
-    const { register, handleSubmit, control } = useForm<OPMproperties>({
-        defaultValues: {
-            status: "draft"
-        }
+const Drawer = ({
+  show,
+  documentId,
+  setActiveDocument
+}: {
+  show: boolean;
+  documentId: String | null;
+  setActiveDocument: any;
+}) => {
+  const { register, handleSubmit, control } = useForm<OPMproperties>({
+    defaultValues: {
+      status: "draft"
+    }
+  });
+
+  const [metadata, setMetadata] = useState({});
+
+  useEffect(() => {
+    if (documentId === null) return;
+    invoke("get_file_properties", { path: documentId as string }).then(
+      (metatada: any) => {
+        if ("propert_groups" in metatada && metatada["propert_groups"].length === 1) {
+            const data: Array<Property> = metadata["property_groups"];
+        //   console.log(documents as File[]);
+        //   setDocuments(documents as File[]);
+      }
+    );
+  }, [documentId]);
+
+  const onSubmit = (data: OPMproperties) => {
+    const metadata: Array<Property> = Object.entries(data).map((datum) => {
+      if (datum[0] === "publish_date") {
+        let prop: Property = {
+          name: datum[0],
+          value: datum[1] ? (datum[1] as Date).toDateString() : ""
+        };
+        return prop;
+      }
+      let prop: Property = { name: datum[0], value: datum[1] as string };
+      return prop;
     });
-    const onSubmit = (data: OPMproperties) => {
 
-        const metadata: Array<Property> = Object.entries(data).map((datum) => {
-            if (datum[0] === "publish_date") {
-                let prop: Property = { name: datum[0], value: datum[1] ? (datum[1] as Date).toDateString() : "" }
-                return prop;
-            }
-            let prop: Property = { name: datum[0], value: datum[1] as string };
-            return prop;
-        })
-
-        if (document) {
-            if (document.property_groups && document.property_groups.length > 0) document.property_groups[0].fields = metadata;
-            invoke('set_file_properties', { target: document.path_lower, properties: metadata });
-        }
-
-        setActiveDocument(null);
-
-    };
-
-    const props = useSpring({
-        left: show ? window.innerWidth - 500 : window.innerWidth,
-        position: "absolute",
-        top: 0,
-        height: "100vh",
-        width: "500px"
-    });
-
-    const hasProperty = document && document.property_groups && document.property_groups.length > 0 && document.property_groups[0];
-
-    const checkForValue = (name: String): string | undefined => {
-        if (!hasProperty) return undefined;
-        let prop = document.property_groups[0].fields.find((property) => { property.name = name as string });
-        if (prop) return prop.value;
-        return undefined;
+    if (document) {
+      if (document.property_groups && document.property_groups.length > 0)
+        document.property_groups[0].fields = metadata;
+      invoke("set_file_properties", {
+        target: document.path_lower,
+        properties: metadata
+      });
     }
 
+    setActiveDocument(null);
+  };
 
-    return (
-        <animated.div
-            style={props as any}
-            className="bg-white border-l-2 border-gray-200 p-4"
+  const props = useSpring({
+    left: show ? window.innerWidth - 500 : window.innerWidth,
+    position: "absolute",
+    top: 0,
+    height: "100vh",
+    width: "500px"
+  });
+
+  const hasProperty =
+    document &&
+    document.property_groups &&
+    document.property_groups.length > 0 &&
+    document.property_groups[0];
+
+  const checkForValue = (name: String): string | undefined => {
+    if (!hasProperty) return undefined;
+    let prop = document.property_groups[0].fields.find((property) => {
+      property.name = name as string;
+    });
+    if (prop) return prop.value;
+    return undefined;
+  };
+
+  return (
+    <animated.div
+      style={props as any}
+      className="bg-white border-l-2 border-gray-200 p-4"
+    >
+      <div className="flex">
+        {document && (
+          <h3 className="grow text-2xl font-bold text-gray-800">
+            {document["name"]}
+          </h3>
+        )}
+        <div
+          className="shrink-0 cursor-pointer"
+          onClick={() => setActiveDocument(null)}
         >
-            <div className="flex">
-                {document && (
-                    <h3 className="grow text-2xl font-bold text-gray-800">
-                        {document["name"]}
-                    </h3>
-                )}
-                <div
-                    className="shrink-0 cursor-pointer"
-                    onClick={() => setActiveDocument(null)}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
-                </div>
-            </div>
-            <form className="mt-10 pr-4" onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label htmlFor="author" className="text-gray-700">
-                        Author
-                    </label>
-                    <input
-                        id="author"
-                        type="text"
-                        className={inputClasses}
-                        {...register("author")}
-                        value={
-                            checkForValue("author")
-                        }
-                    />
-                </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+      </div>
+      <form className="mt-10 pr-4" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="author" className="text-gray-700">
+            Author
+          </label>
+          <input
+            id="author"
+            type="text"
+            className={inputClasses}
+            {...register("author")}
+            value={checkForValue("author")}
+          />
+        </div>
 
-                <div className="mt-4">
-                    <label htmlFor="status" className="text-gray-700">
-                        Status
-                    </label>
-                    <select
-                        id="status"
-                        className={inputClasses}
-                        {...register("status", { required: true })}
-                        value={
-                            checkForValue("status")
-                        }
-                    >
-                        <option value="draft">Draft</option>
-                        <option value="published">Published</option>
-                    </select>
-                </div>
+        <div className="mt-4">
+          <label htmlFor="status" className="text-gray-700">
+            Status
+          </label>
+          <select
+            id="status"
+            className={inputClasses}
+            {...register("status", { required: true })}
+            value={checkForValue("status")}
+          >
+            <option value="draft">Draft</option>
+            <option value="published">Published</option>
+          </select>
+        </div>
 
-                <div className="mt-4">
-                    <label htmlFor="status" className="text-gray-700">
-                        Publish Date
-                    </label>
-                    <Controller
-                        control={control}
-                        name="publish_date"
-                        render={({ field }) => (
-                            <DatePicker
-                                placeholderText="Select date"
-                                onChange={(date) => field.onChange(date)}
-                                className={inputClasses}
-                                selected={
-                                    checkForValue("publish_date") as Date | undefined
-                                }
-                            />
-                        )}
-                    />
-                </div>
+        <div className="mt-4">
+          <label htmlFor="categories" className="text-gray-700">
+            Categories (one per line)
+          </label>
+          <textarea
+            id="categories"
+            className={inputClasses}
+            {...register("categories")}
+            value={checkForValue("categories")}
+          />
+        </div>
 
-                <div className="mt-4">
-                    <label htmlFor="categories" className="text-gray-700">
-                        Categories (one per line)
-                    </label>
-                    <textarea
-                        id="categories"
-                        className={inputClasses}
-                        {...register("categories")}
-                        value={
-                            checkForValue("categories")
-                        }
-                    />
-                </div>
+        <div className="mt-4">
+          <label htmlFor="tags" className="text-gray-700">
+            Tags (one per line)
+          </label>
+          <textarea
+            id="tags"
+            className={inputClasses}
+            {...register("tags")}
+            value={checkForValue("tags")}
+          />
+        </div>
 
-                <div className="mt-4">
-                    <label htmlFor="tags" className="text-gray-700">
-                        Tags (one per line)
-                    </label>
-                    <textarea
-                        id="tags"
-                        className={inputClasses}
-                        {...register("tags")}
-                        value={
-                            checkForValue("tags")
-                        }
-                    />
-                </div>
+        <div className="mt-4">
+          <label htmlFor="template" className="text-gray-700">
+            Display Template
+          </label>
+          <input
+            id="template"
+            type="text"
+            className={inputClasses}
+            {...register("template")}
+            value={checkForValue("template")}
+          />
+        </div>
 
-                <div className="mt-4">
-                    <label htmlFor="template" className="text-gray-700">
-                        Display Template
-                    </label>
-                    <input
-                        id="template"
-                        type="text"
-                        className={inputClasses}
-                        {...register("template")}
-                        value={
-                            checkForValue("template")
-                        }
-                    />
-                </div>
-
-                <input
-                    type="submit"
-                    value="Update Metadata"
-                    className="mt-10 px-4 py-2 hover:cursor-pointer font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
-                />
-            </form>
-        </animated.div>
-    );
+        <input
+          type="submit"
+          value="Update Metadata"
+          className="mt-10 px-4 py-2 hover:cursor-pointer font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+        />
+      </form>
+    </animated.div>
+  );
 };
 
 export default Drawer;
